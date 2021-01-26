fc6de664c6bd1f89af9f46daff529020d4e35b91
Abadila <abadillag7@estudiantec.cr>
2021-01-26 09:24:42 -0600

    Listo Barras Paralelas

--- dataTransformation.py
+++ dataTransformation.py
@@ -17,12 +17,23 @@ if 'Jax' in redSuppts:
 def genCSVParCoods():
     dataMatches = pd.read_csv("./Data/matches2020.csv")
     dataChamps = pd.read_csv("./Data/champion_stats.csv")
-    newCsv = ""
-    for index in dataChamps.index:
-        champ = dataChamps["id"][index]
+    newCsv = "WinRate,Name,Health Points,HP Per Level,Mana Points,MP Per Level,Move Speed,Armor,Magic Resist,Attack Range,Attack Damage,Attack Speed\n"
+    for i in dataChamps.index:
+        champ = dataChamps["id"][i]
+        hp = str(round(dataChamps["hp"][i],3))
+        hpPerLevel = str(round(dataChamps["hpperlevel"][i],3))
+        mp = str(round(dataChamps["mp"][i],3))
+        mpPerLevel = str(round(dataChamps["mpperlevel"][i],3))
+        ms = str(round(dataChamps["movespeed"][i],3))
+        armor = str(round(dataChamps["armor"][i],3))
+        spellblock = str(round(dataChamps["spellblock"][i],3))
+        attackrange = str(round(dataChamps["attackrange"][i],3))
+        ad = str(round(dataChamps["attackdamage"][i],3))
+        attackspeed = str(round(dataChamps["attackspeed"][i],3))
         winRate = calculateWinRate(champ, dataMatches)
-        newCsv = newCsv + champ + "," + str(round(winRate,2)) + "\n"
-    file = open("./Data/winRateByChamp.csv",'w')
+        line = str(round(winRate,2)) + "," + champ + "," + hp + "," + hpPerLevel + "," + mp + "," + mpPerLevel + "," + ms + "," + armor + "," + spellblock + "," + attackrange + "," + ad + "," + attackspeed + "\n"
+        newCsv = newCsv + line
+    file = open("./Data/champ_stats_w_winRate_FINAL.csv",'w')
     file.write(newCsv)
     file.close()
 
@@ -30,35 +41,35 @@ def calculateWinRate(champ, dataMatches):
     winCant = 0
     pickCant = 0
 
-    for index in dataMatches.index:
-        rTop = dataMatches["redtop"][index]
-        rJg = dataMatches["redjungle"][index]
-        rMid = dataMatches["redmid"][index]
-        rAdc = dataMatches["redadc"][index]
-        rSupp = dataMatches["redsupport"][index]
-        bTop = dataMatches["bluetop"][index]
-        bJg = dataMatches["bluejungle"][index]
-        bMid = dataMatches["bluemid"][index]
-        bAdc = dataMatches["blueadc"][index]
-        bSupp = dataMatches["bluesupport"][index]
-        win = dataMatches["result"][index]
+    for i in dataMatches.index:
+        rTop = dataMatches["redtop"][i]
+        rJg = dataMatches["redjungle"][i]
+        rMid = dataMatches["redmid"][i]
+        rAdc = dataMatches["redadc"][i]
+        rSupp = dataMatches["redsupport"][i]
+        bTop = dataMatches["bluetop"][i]
+        bJg = dataMatches["bluejungle"][i]
+        bMid = dataMatches["bluemid"][i]
+        bAdc = dataMatches["blueadc"][i]
+        bSupp = dataMatches["bluesupport"][i]
+        win = dataMatches["result"][i]
         if (champ == rTop or champ == rJg or champ == rMid or champ == rAdc or champ == rSupp) and (win == 0):
             winCant = winCant + 1
         elif (champ == bTop or champ == bJg or champ == bMid or champ == bAdc or champ == bSupp) and (win == 1):
             winCant = winCant + 1
 
-    for index in dataMatches.index:
-        rTop = dataMatches["redtop"][index]
-        rJg = dataMatches["redjungle"][index]
-        rMid = dataMatches["redmid"][index]
-        rAdc = dataMatches["redadc"][index]
-        rSupp = dataMatches["redsupport"][index]
-        bTop = dataMatches["bluetop"][index]
-        bJg = dataMatches["bluejungle"][index]
-        bMid = dataMatches["bluemid"][index]
-        bAdc = dataMatches["blueadc"][index]
-        bSupp = dataMatches["bluesupport"][index]
-        win = dataMatches["result"][index]
+    for i in dataMatches.index:
+        rTop = dataMatches["redtop"][i]
+        rJg = dataMatches["redjungle"][i]
+        rMid = dataMatches["redmid"][i]
+        rAdc = dataMatches["redadc"][i]
+        rSupp = dataMatches["redsupport"][i]
+        bTop = dataMatches["bluetop"][i]
+        bJg = dataMatches["bluejungle"][i]
+        bMid = dataMatches["bluemid"][i]
+        bAdc = dataMatches["blueadc"][i]
+        bSupp = dataMatches["bluesupport"][i]
+        win = dataMatches["result"][i]
         if (champ == rTop or champ == rJg or champ == rMid or champ == rAdc or champ == rSupp or champ == bTop or champ == bJg or champ == bMid or champ == bAdc or champ == bSupp):
             pickCant = pickCant + 1
     
@@ -67,5 +78,33 @@ def calculateWinRate(champ, dataMatches):
     else:
         return 0
 
-        
+def genCSVSpider():
+    dataWorlds = pd.read_csv("./Data/Worlds 2020 Main Event - Team Stats - OraclesElixir.csv")
+    newCsv = "Team,FB%,FT%,F3T%,HLD%,FD%,DRG%,ELD%,BN%\n"
+    for i in dataWorlds.index:
+        # get data from csv
+        team = dataWorlds["Team"][i]
+        fb = dataWorlds["FB%"][i]
+        fb = fb[:-1] # remove %
+        ft = dataWorlds["FT%"][i]
+        ft = ft[:-1] # remove %
+        f3t = dataWorlds["F3T%"][i]
+        f3t = f3t[:-1] # remove %
+        hld = dataWorlds["HLD%"][i]
+        hld = hld[:-1] # remove %
+        fd = dataWorlds["FD%"][i]
+        fd = fd[:-1] # remove %
+        drg = dataWorlds["DRG%"][i]
+        drg = drg[:-1] # remove %
+        eld = dataWorlds["ELD%"][i]
+        eld = eld[:-1] # remove %
+        bn = dataWorlds["BN%"][i]
+        bn = bn[:-1] # remove %
+        line = team + "," + fb + "," + ft + "," + f3t + "," + hld + "," + fd + "," + drg + "," + eld + "," + bn + "\n"
+        newCsv = newCsv + line
+    file = open("./Data/WorldsTeamStats_FINAL.csv",'w')
+    file.write(newCsv)
+    file.close()
+
 genCSVParCoods()
+genCSVSpider()
--- gráficoCoordenadas.js
+++ gráficoCoordenadas.js
@@ -1,5 +1,111 @@
+//Variables globales
+//----------------------------------------------------
+
+
+
+
+//----------------------------------------------------
+//Se precargan los datos para almacenarlos en variables
+//----------------------------------------------------
+function ocultar(){
+    document.getElementById('seleccionar').style.display = 'none';
+  }
+
 function getData(){
-    d3.csv("./Data/matches2020.csv", (data)=>{
+    d3.csv("./Data/WorldsTeamStats_FINAL.csv", (data)=>{
         console.log(data)
     });
 }
+
+function visParCoords(){
+  document.getElementById("container").innerHTML = "";
+  document.getElementById("grid").innerHTML = "";
+  document.getElementById('container').setAttribute( "class", "parcoords" );
+  document.getElementById('grid').setAttribute( "class", "scrollable" );
+    //se generan las lineas del grafico con el color seleccionado
+    var parcoords = d3.parcoords()("#container")
+    .lineWidth(1)
+    .alpha(0.7);
+
+    var blueToRed = ["#13a5de","#e42d29"]
+
+    d3.csv("./Data/champ_stats_w_winRate_FINAL.csv", function(data){
+        //filtra las llaves que son numericas para obtener la primera
+        var numericKeys = d3.keys(data[0]).filter(function(d){ //d is the key
+            var valuesByKey = data.map(x=>x[d]);
+            var keyType = (valuesByKey.some(isNaN) ? "categorical" : "numeric");
+            if(keyType == "numeric"){
+              return d;
+            };  
+          });
+        var firstNumericKey = numericKeys[0]; //llave de la primera columna numerica
+        parcoords
+            .data(data)
+            .render()
+            .reorderable()
+            .brushMode("1D-axes")
+            .interactive();
+        
+        change_color(firstNumericKey,parcoords.data());
+
+        var grid = d3.divgrid();
+        d3.select("#grid")
+          .datum(data)
+          .call(grid)
+          .selectAll(".row")
+          .on({
+            "mouseover": function (d) { parcoords.highlight([d]); },
+            "mouseout": parcoords.unhighlight
+          });
+
+          parcoords.on("brush", function (d) {
+            d3.select("#grid")
+              .datum(d)
+              .call(grid)
+              .selectAll(".row")
+              .on({
+                "mouseover": function (d) { parcoords.highlight([d]); },
+                "mouseout": parcoords.unhighlight
+              });
+          });
+
+        
+    });
+
+    // update color
+    function change_color(dimension,data) {
+        //filtra las llaves que son numericas para obtener la primera
+        var numericKeys = d3.keys(data[0]).filter(function(d){ //d is the key
+        var valuesByKey = data.map(x=>x[d]);
+        var keyType = (valuesByKey.some(isNaN) ? "categorical" : "numeric");
+        if(keyType == "numeric"){
+            return d;
+        };  
+        });
+        if(numericKeys.includes(dimension)){
+        parcoords.svg.selectAll(".dimension")
+            .style("font-weight", "normal")
+            .filter(function(d) { return d == dimension; })
+            .style("font-weight", "bold")
+        var actualNumericColumn = data.map(x=>x[dimension]).map(returnFloat);
+        var maxIndex = Math.min(...actualNumericColumn); //valor minimo de esa columna
+        var minIndex = Math.max(...actualNumericColumn); //valor maximo de esa columna
+        var pltt_1 = d3.scale.linear()
+            .domain([minIndex, maxIndex])
+            .range(blueToRed)
+            .interpolate(d3.interpolateLab);
+
+        var clr = function (d) { return pltt_1(d[dimension])};
+
+        parcoords.color(clr).render()
+        } else {
+        alert("No se puede colorear con esta columna, uno o más datos no son numéricos");
+        }
+    }
+
+    //funcion para mapear a enteros
+    function returnFloat(element){
+        return parseFloat(element,10);
+    }
+}
+
