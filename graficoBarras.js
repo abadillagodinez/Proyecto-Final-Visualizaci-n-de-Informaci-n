b36c4cca82adb3d529e43a7fe4e0ada08b841e21
eriraldo <ericksalazar_g@hotmail.com>
2021-01-28 12:06:36 -0600

    Se agrega una interfaz para el gráfico de barras
    se integra el grafico de barras a la pagina principal sin errores y solo se tiene que cambiar el CSV y agregarle alguna interactividad

--- graficoBarras.js
+++ graficoBarras.js
@@ -1,191 +1,105 @@
+var flag = true;
 function vis(){
-  var svg = d3.select("svg"),
-  margin = {top: 20, right: 20, bottom: 30, left: 40},
-  width = +svg.attr("width") - margin.left - margin.right,
-  height = +svg.attr("height") - margin.top - margin.bottom,
-  g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    if(flag == true){
+    var margin = {top: 20, right: 20, bottom: 30, left: 0},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-// The scale spacing the groups:
-var x0 = d3.scaleBand()
-  .rangeRound([0, width])
-  .paddingInner(0.1);
+var x0 = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
 
-// The scale for spacing each group's bar:
-var x1 = d3.scaleBand()
-  .padding(0.05);
+var x1 = d3.scale.ordinal();
 
-var y = d3.scaleLinear()
-  .rangeRound([height, 0]);
+var y = d3.scale.linear()
+    .range([height, 0]);
 
-var z = d3.scaleOrdinal()
-  .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-d3.csv("data.csv", function(d, i, columns) {
-  for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = +d[columns[i]];
-  return d;
-}).then(function(data) {
-  //console.log(data);
+var xAxis = d3.svg.axis()
+    .scale(x0)
+    .orient("bottom");
 
-  var keys = data.columns.slice(1);
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickFormat(d3.format(".2s"));
 
-  //console.log('keys');
-  //console.log(keys);
+var svg = d3.select("#barras").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .style("fill","white")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("data.csv", function(error, data) {
+  if (error) throw error;
+
+  var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "State"; });
+
+  data.forEach(function(d) {
+    d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
+  });
+
   x0.domain(data.map(function(d) { return d.State; }));
-  x1.domain(keys).rangeRound([0, x0.bandwidth()]);
-  y.domain([0, d3.max(data, function(d) { return d3.max(keys, function(key) { return d[key]; }); })]).nice();
+  x1.domain(ageNames).rangeRoundBands([0, x0.rangeBand()]);
+  y.domain([0, d3.max(data, function(d) { return d3.max(d.ages, function(d) { return d.value; }); })]);
 
-  g.append("g")
-      .selectAll("g")
-      .data(data)
-      .enter().append("g")
-      .attr("class","bar")
-      .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; })
-      .selectAll("rect")
-      .data(function(d) { return keys.map(function(key) { return {key: key, value: d[key]}; }); })
-      .enter().append("rect")
-      .attr("x", function(d) { return x1(d.key); })
-      .attr("y", function(d) { return y(d.value); })
-      .attr("width", x1.bandwidth())
-      .attr("height", function(d) { return height - y(d.value); })
-      .attr("fill", function(d) { return z(d.key); });
-
-  g.append("g")
-      .attr("class", "axis")
+  svg.append("g")
+      .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x0));
+      .style("fill","white")
+      .call(xAxis);
 
-  g.append("g")
+  svg.append("g")
       .attr("class", "y axis")
-      .call(d3.axisLeft(y).ticks(null, "s"))
-      .append("text")
-      .attr("x", 2)
-      .attr("y", y(y.ticks().pop()) + 0.5)
-      .attr("dy", "0.32em")
-      .attr("fill", "#000")
-      .attr("font-weight", "bold")
-      .attr("text-anchor", "start")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .style("fill","white")
       .text("Population");
 
-  var legend = g.append("g")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-      .attr("text-anchor", "end")
-      .selectAll("g")
-      .data(keys.slice().reverse())
-      .enter().append("g")
+  var state = svg.selectAll(".state")
+      .data(data)
+    .enter().append("g")
+      .attr("class", "g")
+      .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; });
+
+  state.selectAll("rect")
+      .data(function(d) { return d.ages; })
+    .enter().append("rect")
+      .attr("width", x1.rangeBand())
+      .attr("x", function(d) { return x1(d.name); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); })
+      .style("fill", function(d) { return color(d.name); });
+
+  var legend = svg.selectAll(".legend")
+      .data(ageNames.slice().reverse())
+    .enter().append("g")
+      .attr("class", "legend")
       .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
   legend.append("rect")
-      .attr("x", width - 17)
-      .attr("width", 15)
-      .attr("height", 15)
-      .attr("fill", z)
-      .attr("stroke", z)
-      .attr("stroke-width",2)
-      .on("click",function(d) { update(d) });
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
 
   legend.append("text")
       .attr("x", width - 24)
-      .attr("y", 9.5)
-      .attr("dy", "0.32em")
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .style("fill","white")
       .text(function(d) { return d; });
 
-  var filtered = [];
-
-  ////
-  //// Update and transition on click:
-  ////
-
-  function update(d) {
-
-      //
-      // Update the array to filter the chart by:
-      //
-
-      // add the clicked key if not included:
-      if (filtered.indexOf(d) == -1) {
-          filtered.push(d);
-          // if all bars are un-checked, reset:
-          if(filtered.length == keys.length) filtered = [];
-      }
-      // otherwise remove it:
-      else {
-          filtered.splice(filtered.indexOf(d), 1);
-      }
-
-      //
-      // Update the scales for each group(/states)'s items:
-      //
-      var newKeys = [];
-      keys.forEach(function(d) {
-          if (filtered.indexOf(d) == -1 ) {
-              newKeys.push(d);
-          }
-      })
-      x1.domain(newKeys).rangeRound([0, x0.bandwidth()]);
-      y.domain([0, d3.max(data, function(d) { return d3.max(keys, function(key) { if (filtered.indexOf(key) == -1) return d[key]; }); })]).nice();
-
-      // update the y axis:
-      svg.select(".y")
-          .transition()
-          .call(d3.axisLeft(y).ticks(null, "s"))
-          .duration(500);
-
-
-      //
-      // Filter out the bands that need to be hidden:
-      //
-      var bars = svg.selectAll(".bar").selectAll("rect")
-          .data(function(d) { return keys.map(function(key) { return {key: key, value: d[key]}; }); })
-
-      bars.filter(function(d) {
-              return filtered.indexOf(d.key) > -1;
-          })
-          .transition()
-          .attr("x", function(d) {
-              return (+d3.select(this).attr("x")) + (+d3.select(this).attr("width"))/2;
-          })
-          .attr("height",0)
-          .attr("width",0)
-          .attr("y", function(d) { return height; })
-          .duration(500);
-
-      //
-      // Adjust the remaining bars:
-      //
-      bars.filter(function(d) {
-              return filtered.indexOf(d.key) == -1;
-          })
-          .transition()
-          .attr("x", function(d) { return x1(d.key); })
-          .attr("y", function(d) { return y(d.value); })
-          .attr("height", function(d) { return height - y(d.value); })
-          .attr("width", x1.bandwidth())
-          .attr("fill", function(d) { return z(d.key); })
-          .duration(500);
-
-
-      // update legend:
-      legend.selectAll("rect")
-          .transition()
-          .attr("fill",function(d) {
-              if (filtered.length) {
-                  if (filtered.indexOf(d) == -1) {
-                      return z(d);
-                  }
-                  else {
-                      return "white";
-                  }
-              }
-              else {
-                  return z(d);
-              }
-          })
-          .duration(100);
-
-
-  }
-
 });
-
+    }
+    else{
+        
+    }
 }
